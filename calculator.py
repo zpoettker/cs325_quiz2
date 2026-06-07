e51ca46008af17966482dd912816ba9f9a2216e5
zpoettker <poettkerzach@gmail.com>
2026-06-07 18:16:12 +0000

    fixed calc

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 def add(x, y): 
-  return x + y
+  print(x + y)
 
 def sub(x, y):
-  return x - y
+  print(x - y)
 
-num1 = input("Type your first number")
-addorsub = input("+ or - ?")
-num2 = input("Type your first number")
+num1 = float(input("Type your first number: "))
+addorsub = input("+ or - ?: ")
+num2 = float(input("Type your first number: "))
 
 if(addorsub == "+"):
   add(num1, num2)
