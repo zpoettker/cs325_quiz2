2066f90b077e44832190827204684a6ceadf4148
zpoettker <poettkerzach@gmail.com>
2026-06-07 18:51:12 +0000

    added mult & div to readme and code

--- calculator.py
+++ calculator.py
@@ -4,14 +4,24 @@ def add(x, y):
 def sub(x, y):
   print(x - y)
 
+def mult(x, y): 
+  print(x * y)
+
+def div(x, y):
+  print(x / y)
+
 num1 = float(input("Type your first number: "))
-addorsub = input("+ or - ?: ")
-num2 = float(input("Type your first number: "))
+addorsub = input("Type +, -, *, or /: ")
+num2 = float(input("Type your second number: "))
 
 if(addorsub == "+"):
   add(num1, num2)
 elif(addorsub == "-"):
   sub(num1, num2)
+elif(addorsub == "*"):
+  mult(num1, num2)
+elif(addorsub == "-"):
+  div(num1, num2)
 else:
   printf("Incorrect format try again!")
 
